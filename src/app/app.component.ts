5d2066c67c7dc740e3c3b5d70ffb7f2773da30a5
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-04 05:11:16 +0300

    feat [angular]: implement add/remove todo logic; add validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const todos = [
+  { id: 1, title: 'TypeScript', completed: true },
+  { id: 2, title: 'React', completed: false },
+  { id: 3, title: 'Angular', completed: true },
+];
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  editing = false;
+  todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(3)],
+    }),
+  });
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+
+  get activeTodos() {
+    return this.todos.filter((todo) => !todo.completed);
+  }
+
+  addTodo() {
+    if (this.todoForm.invalid) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title.value as string,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
