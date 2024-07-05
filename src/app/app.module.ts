a3f0f3573e0978b3744e49a753f757cc9d0c735f
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-05 03:16:54 +0300

    refactor [angular]: move active todos filtering to pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { FilterActivePipe } from './pipes/filter-active.pipe';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent, TodoFormComponent],
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    TodoFormComponent,
+    FilterActivePipe,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
